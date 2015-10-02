93b3692cdae498874425245a0b25410f2299c151
zhangbohun <zhangbohun@163.com>
2015-10-02 21:40:53 +0800

    修改

--- 0001/0001.py
+++ 0001/0001.py
@@ -10,7 +10,7 @@ def generate_activation_code(count):
     code_list = []
     for i in xrange(count):
         code = str(uuid.uuid4()).upper()
-        if not code in code_list:#唯一性是关键，这里用uuid基本不会有重复的可能
+        if code not in code_list:#唯一性是关键，这里用uuid基本不会有重复的可能
         #但是如果使用随机数实现的话就不一定了，除了用not in来解决以外，也可以用set()来存储结果，然后判断是否够200个
             code_list.append(code)
     return code_list
