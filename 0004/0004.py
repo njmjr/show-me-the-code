d499b7239aa50366f577e0e03b07347df12a286d
zhangbohun <zhangbohun@163.com>
2015-10-04 13:30:56 +0800

    两题

---
+++ 0004/0004.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 04 12:30:18 2015
+
+@author: zhangbohun
+"""
+import re
+
+def count_word(filepath):
+    with open(filepath,'rb')as f:
+        txt = f.read()
+    words = re.findall(r'[a-zA-Z0-9]+', txt)#[a-zA-Z0-9]+或者[\w]+
+#    dic = {}
+#    for word in words:
+#        if word is '':
+#            continue
+#        if word in dic:
+#            dic[word] += 1
+#        else:
+#            dic[word] = 1
+#    return dic
+    return len(words)
+
+if __name__ == '__main__':
+    print count_word('English.txt')
---
+++ 0005/0005.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 04 12:52:24 2015
+
+@author: zhangbohun
+"""
+from PIL import Image
+import os
+
+def reSize(dirPath, ResizeX = 100, ResizeY = 100):
+    picturelist=os.listdir(dirPath)
+    for picture in picturelist:
+        #print picture
+        img = Image.open(dirPath+picture)
+        x,y=img.size
+        if x<ResizeX:
+            tmp_X=x
+        else:
+            tmp_X=ResizeX
+        if y<ResizeY:
+            tmp_Y=y  
+        else:
+            tmp_Y=ResizeY
+        new = img.resize((tmp_X, tmp_Y))
+        new.save(picture.split('.')[0] + '_modifid' + '.png')
+
+if __name__ == '__main__':
+    reSize('img/',  640,1136)
